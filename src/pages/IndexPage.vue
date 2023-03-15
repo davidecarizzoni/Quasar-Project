fb8b43d66ef6f127ef07ec9efb685ea46130c741
Davide Carizzoni <davide.carizzoni@runelab.it>
2023-03-15 22:55:17 +0100

    feat(i18n): setup

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,11 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <div class="row full-width justify-center">
+      {{$t('welcome')}}
+    </div>
+    <div class="row full-width justify-center">
+      {{$t('welcomeWithUsername', { username: 'Davide'})}}
+    </div>
   </q-page>
 </template>
 
