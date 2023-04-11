740fa503b33c6a7b32d54f805b069e0b7d8953ca
Davide Carizzoni <davide.carizzoni@runelab.it>
2023-04-11 21:03:51 +0200

    fix(many)

--- src/store/user/actions.js
+++ src/store/user/actions.js
@@ -1,25 +1,18 @@
 import {api} from "boot/axios";
 
 export async function login ({ commit }, payload) {
-  try {
-    const { email, password } = payload
-    try {
-      const { data } = await api.post('auth', undefined, {
-        headers: {
-          Authorization: 'Basic ' + btoa(email + ':' + password)
-        }
-      })
-      // The btoa() method creates a Base64-encoded ASCII string from a binary string
-      // (i.e., a string in which each character in the string is treated as a byte of binary data).
-      commit('setToken', data.token)
-      commit('setDetails', data.user)
-      return Promise.resolve(data.user)
-    } catch (e) {
-      return Promise.reject(e)
+  const { email, password } = payload
+  const { data } = await api.post('auth', undefined, {
+    headers: {
+      Authorization: 'Basic ' + btoa(email + ':' + password)
     }
-  } catch (e) {
-    console.error({e})
-  }
+  })
+  // The btoa() method creates a Base64-encoded ASCII string from a binary string
+  // (i.e., a string in which each character in the string is treated as a byte of binary data).
+  commit('setToken', data.token)
+  commit('setDetails', data.user)
+  return data.user 
+
 }
 
 export async function logout ({ commit }) {
